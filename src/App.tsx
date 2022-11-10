4a2d3ec3636311c49a9d65e7d06c561186ce3698
北沢亮和 <ryowa.kitazawa@gmail.com>
2022-11-10 23:57:10 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [status, setStatus] = useState("text");
+  const [input, setInput] = useState("");
+  const [counter, setCounter] = useState(0);
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  useEffect(() => {
+    console.log("aaaaa");
+    document.title = `curent value is ${counter}`;
+  },[counter])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h4>{status}</h4>
+        <button onClick={() => setStatus("new text")}>Button</button>
+        <h4>{input}</h4>
+        <input type="text" value={input} onChange={onChangeHandler} />
+        <h4>{counter}</h4>
+        <button
+          onClick={() => setCounter((preCounter) => preCounter + 1)}
+        >
+          Increment
+        </button>
         <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
     </div>
   );
-}
+};
 
 export default App;
